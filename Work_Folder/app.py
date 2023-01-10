beb1170d8997adf82412dcb3b9f94019acce67e4
Jesse Huang <jesshuang5733@gmail.com>
2023-01-10 14:59:27 -0500

    Update app.py

--- Work_Folder/app.py
+++ Work_Folder/app.py
@@ -7,8 +7,8 @@ app = Flask(__name__, template_folder='Templates')
 @app.route('/test', methods=['POST'])
 def testfn():
     print(request.get_json())  # parse as JSON
-    data = request.get_json()
-    return data, 200
+    data = request.get_json() 
+    return data, 200 #change data to whatever variable needed (currently just returns the data from post req)
  
 if __name__ == "__main__":
     app.run(
