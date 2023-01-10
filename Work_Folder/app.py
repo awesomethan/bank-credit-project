215cbdd24053cff9372d45e37678e3450cd82e8f
Jesse Huang <jesshuang5733@gmail.com>
2023-01-10 14:38:12 -0500

    Update app.py

--- Work_Folder/app.py
+++ Work_Folder/app.py
@@ -2,21 +2,13 @@ from flask import Flask, render_template
 from flask import request
 from flask import jsonify
 
-temp = {"help": "me", "ads": "dhbf"}
-
 app = Flask(__name__, template_folder='Templates')
 
 @app.route('/test', methods=['POST'])
-def testft():
-  
-    print(request.get_json())  # parse as JSON
-    return 'Sucesss', 200
-
-@app.route('/test2', methods=['GET'])
 def testfn():
-    message = temp
-    return jsonify(message)  # serialize and use JSON headers
-
+    print(request.get_json())  # parse as JSON
+    data = request.get_json()
+    return data, 200
  
 if __name__ == "__main__":
     app.run(
